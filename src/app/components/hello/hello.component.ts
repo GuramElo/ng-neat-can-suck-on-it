8f35d09b2c2ef51db6c2e72d1df3e0985c1812b9
Guram Elizbarashvili <elizbarashvili.guram18@gmail.com>
2023-06-11 01:31:15 +0400

    V1.1.0

--- src/app/components/hello/hello.component.ts
+++ src/app/components/hello/hello.component.ts
@@ -1,63 +1,8 @@
-import {
-  ChangeDetectionStrategy,
-  Component,
-  OnDestroy,
-  OnInit,
-} from '@angular/core';
-import {
-  MonoTypeOperatorFunction,
-  Observable,
-  ObservableInput,
-  Subject,
-  fromEvent,
-  noop,
-  of,
-  timer,
-} from 'rxjs';
-import { operate } from 'rxjs/internal/util/lift';
-import { createOperatorSubscriber } from 'rxjs/internal/operators/OperatorSubscriber';
-import { innerFrom } from 'rxjs/internal/observable/innerFrom';
-interface DestroyBase {
-  ___$destroyRef___?: Observable<void>;
-  ngOnDestroy?: () => void;
-  [key: string]: any;
-}
-function UntilDestroy(): ({ prototype }: {prototype: DestroyBase}) => void {
-  return ({ prototype: instance }: { prototype: DestroyBase }) => {
-    //const instance: DestroyBase = prototype;
-    const originalOnDestroy: () => void =
-      (instance.ngOnDestroy)?.bind?.(instance) ?? (() => void 0);
-    // const originalOnInit: () => void = instance.ngOnInit?.bind?.(instance);
-    const ___$destroyRef___ = new Subject<void>();
-
-    Object.assign(instance, {
-      ngOnDestroy: (() => {
-        ___$destroyRef___.next();
-        originalOnDestroy();
-      }).bind(instance),
-    });
-    Object.defineProperty<DestroyBase>(
-      instance,
-      '___$destroyRef___',
-      {
-        value: ___$destroyRef___.asObservable(),
-        writable: false,
-        enumerable: false,
-        configurable: false,
-      }
-    );
-  };
-}
-export function takeUntilDestroyed<T = void>(value: DestroyBase): MonoTypeOperatorFunction<T> {
-  const notifier: ObservableInput<void> = value.___$destroyRef___ ?? of(void 0);
-  return operate((source, subscriber) => {
-    innerFrom(notifier).subscribe(
-      createOperatorSubscriber(subscriber, () => subscriber.complete(), noop)
-    );
-    !subscriber.closed && source.subscribe(subscriber);
-  });
-}
 //====================================================================================
+import {takeUntilDestroyed, UntilDestroy} from "../../../decorators/ng-neaat-can-suck-on-this";
+import {ChangeDetectionStrategy, Component, OnDestroy, OnInit} from "@angular/core";
+import {fromEvent, timer} from "rxjs";
+
 @UntilDestroy()
 @Component({
   selector: 'app-hello',
@@ -65,7 +10,7 @@ export function takeUntilDestroyed<T = void>(value: DestroyBase): MonoTypeOperat
   styleUrls: ['./hello.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HelloComponent {
+export class HelloComponent implements OnInit, OnDestroy {
   pura: any = 123123123123;
   pura123123: any = 'asdasdasdasdasd';
   fula$: any = timer(0, 1000)
@@ -86,5 +31,8 @@ export class HelloComponent {
           console.log(compplete, 'completeee')
       },
     });
+  }
+  ngOnDestroy() {
+    console.log('destroyyyy')
   }
 }
---
+++ src/decorators/ng-neaat-can-suck-on-this.ts
@@ -0,0 +1,52 @@
+import {
+  MonoTypeOperatorFunction,
+  Observable,
+  ObservableInput,
+  Subject,
+  noop,
+  of,
+  take,
+} from 'rxjs';
+import { operate } from 'rxjs/internal/util/lift';
+import { createOperatorSubscriber } from 'rxjs/internal/operators/OperatorSubscriber';
+import { innerFrom } from 'rxjs/internal/observable/innerFrom';
+interface DestroyBase {
+  ___$destroyRef___?: Observable<void>;
+  ngOnDestroy?: () => void;
+  [key: string]: any
+}
+export function UntilDestroy(): ({ prototype }: {prototype: DestroyBase}) => void {
+  return ({ prototype: instance }: { prototype: DestroyBase }) => {
+    //const instance: DestroyBase = prototype;
+    const originalOnDestroy: () => void =
+      (instance.ngOnDestroy)?.bind?.(instance) ?? (() => void 0);
+    // const originalOnInit: () => void = instance.ngOnInit?.bind?.(instance);
+    const ___$destroyRef___ = new Subject<void>();
+
+    Object.assign(instance, {
+      ngOnDestroy: (function() {
+        ___$destroyRef___.next();
+        originalOnDestroy();
+      }).bind(instance),
+    });
+    Object.defineProperty<DestroyBase>(
+      instance,
+      '___$destroyRef___',
+      {
+        value: ___$destroyRef___.asObservable().pipe(take(1)),
+        writable: false,
+        enumerable: false,
+        configurable: false,
+      }
+    );
+  };
+}
+export function takeUntilDestroyed<T = void>({___$destroyRef___}: DestroyBase): MonoTypeOperatorFunction<T> {
+  const notifier: ObservableInput<void> = ___$destroyRef___ ?? of(void 0);
+  return operate((source, subscriber) => {
+    innerFrom(notifier).subscribe(
+      createOperatorSubscriber(subscriber, () => subscriber.complete(), noop)
+    );
+    !subscriber.closed && source.subscribe(subscriber);
+  });
+}
