d9c61294f0cb6fac5d41dd44658c07fd9550900f
Gurami Elizbarashvili <gelizbarashvili@crocobet.com>
2023-06-13 11:54:14 +0400

    solution#2

--- src/app/child1/child1.component.ts
+++ src/app/child1/child1.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { JsonPipe } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-child1',
+  imports: [JsonPipe],
   templateUrl: './child1.component.html',
   styleUrls: ['./child1.component.scss'],
   standalone: true
 })
 export class Child1Component {
+  @Input() set hello(param: any) {
+    if ((param ?? null) === null) {
+      return;
+    }
+    console.log(param, '1111')
+    this.updateValue(param)
+  }
   value: any;
   updateValue(args:any): void {
     this.value = args;
--- src/app/mother/mother.component.ts
+++ src/app/mother/mother.component.ts
@@ -1,13 +1,23 @@
-import { Component } from '@angular/core';
+import { AsyncPipe, NgTemplateOutlet } from '@angular/common';
+import { Component, Input, TemplateRef } from '@angular/core';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-mother',
   templateUrl: './mother.component.html',
+  imports: [AsyncPipe, NgTemplateOutlet],
   styleUrls: ['./mother.component.scss'],
   standalone: true
 })
 export class MotherComponent {
+  @Input() tut!: TemplateRef<any>;
+
+  public readonly $sub = new Subject();
+
 public handleClick() {
-  console.log('do smth')
+  // console.log('do smth')
+  this.$sub.next({
+    hello: Math.floor(Math.random()* 100)
+  })
 }
 }
